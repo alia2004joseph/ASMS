26f0564d8c43d18cb09253d1565f3ba59073afff
alia2004joseph <1professor2004@gmail.com>
2026-08-15 18:42:12 +0300

    modified apps.py

---
+++ class_management/representatives/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class RepresentativesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "class_management.representatives"
+    verbose_name = "Class Management - Representatives"
